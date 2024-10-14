efd495166f2b2b6e91e734105b54228b5c1cf2c6
Mike Sindani <mikems@live.fr>
2024-10-14 22:45:48 +0200

    enlever l'importation de localfont

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/Footer/Footer";
 import { Dosis , Poppins} from '@next/font/google';
